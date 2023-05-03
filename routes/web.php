aa31260b337e568b7830e13ec99e251103020d0b
cepiyoga <cepiyoga@gmail.com>
2023-05-03 22:39:55 +0700

    Data 3 May

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Cepi Yoga Asmara",
+        "email" => "cepiyoga@gmail.com",
+        "image" => "cepi.jpg"
+    ]);
+});
+
+
+
+Route::get('/blog', function () {
+    return view('posts');
+});
