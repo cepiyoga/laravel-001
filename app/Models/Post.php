e6e71b8a13bf4131c15c59e99153e659221613fb
cepiyoga <cepiyoga@gmail.com>
2023-05-04 17:17:52 +0700

    Tambah Model dan postController

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,7 @@ namespace App\Models;
 
 
 
-class Post
-{
+class Post{
     private static $blog_post = [[
         "title" => "Judul Post Pertama",
         "slug" => "judul-post-pertama",
@@ -18,4 +17,17 @@ class Post
         "author" => "Naufal Yoga",
         "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem reprehenderit in pariatur! Quis dignissimos eius facilis iure itaque omnis iusto esse blanditiis numquam ducimus, nulla ullam fuga fugit, fugiat nobis pariatur, et exercitationem necessitatibus. Nobis, illo, ullam atque harum accusamus culpa non debitis deserunt, reprehenderit ex adipisci? Nesciunt perspiciatis itaque perferendis obcaecati blanditiis quod qui, voluptatum dolorum doloremque non consequatur maiores laboriosam facilis totam, veniam recusandae suscipit ratione! Sit magni suscipit mollitia animi incidunt nam unde odio omnis optio nulla. Impedit, deleniti totam! Consectetur, consequatur maxime neque, accusamus reprehenderit sunt iste doloribus veritatis quo molestias perspiciatis magni dolore dolor laboriosam."
     ]];
-}
+
+    public static function all(){
+        return collect(self::$blog_post);
+    }
+
+    public static function find($slug)
+    {
+        $posts = static::all();
+        return $posts->firstWhere('slug',$slug);
+
+    }
+};
+
+
